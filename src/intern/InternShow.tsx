565379e7c9f5aab684e471c75f79f3be4e531f78
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-06-02 15:31:05 +0300

    feat(intern): implement InternShow with ReferenceField and ManagerCard integration

--- src/intern/InternShow.tsx
+++ src/intern/InternShow.tsx
@@ -1,7 +1,35 @@
+import {
+  BooleanField,
+  NumberField,
+  ReferenceField,
+  Show,
+  SimpleShowLayout,
+  TextField,
+  FunctionField,
+} from "react-admin";
+import { ManagerCard } from "../components/ManegerCard";
 
+export const InternShow = () => (
+  <Show>
+    <SimpleShowLayout>
+      <TextField source="id" />
+      <TextField source="firstName" label="Prénom" />
+      <TextField source="lastName" label="Nom" />
+      <BooleanField source="isRemunerate" label="Rémunéré" />
+      <NumberField source="remuneration" label="Rémunération" />
 
-const InternShow = () => (
-  <div>Intern Show</div>
-)
+      <ReferenceField
+        source="managerId"
+        reference="employees"
+        link="show"
+        label="Lien Manager"
+      >
+        <FunctionField
+          render={(record?: any) => record ? `${record.firstName} ${record.lastName}` : ''}
+        />
+      </ReferenceField>
 
-export default InternShow;
+      <ManagerCard />
+    </SimpleShowLayout>
+  </Show>
+);
